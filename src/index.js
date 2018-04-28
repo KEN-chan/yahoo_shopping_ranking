7652e7a14c73aa2edb7c11353a8862e0c7d86c10
KEN-chan <hellokenta1024@gmail.com>
2018-04-28 16:42:36 -0400

    install react router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger'; 
+import { Provider } from 'react-redux';
+import * as reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  combineReducers(reducers),
+  applyMiddleware(logger),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
